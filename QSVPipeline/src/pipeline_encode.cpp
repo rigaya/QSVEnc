f3452c87a480bc5f42904f5f2aad626685caabaa
rigaya <rigaya34589@live.jp>
2015-07-20 08:57:17 +0900

    ログ表示を短縮

--- QSVPipeline/src/pipeline_encode.cpp
+++ QSVPipeline/src/pipeline_encode.cpp
@@ -423,7 +423,7 @@ mfxStatus CEncodingPipeline::InitMfxEncParams(sInputParams *pInParams)
     };
     //エンコードモードのチェック
     mfxU64 availableFeaures = CheckEncodeFeature(pInParams->bUseHWLib, m_mfxVer, pInParams->nEncMode, pInParams->CodecId);
-    PrintMes(QSV_LOG_DEBUG, _T("Detected avaliable features for %s API v%d.%d, codec %s, mode %s\n%s\n"),
+    PrintMes(QSV_LOG_DEBUG, _T("Detected avaliable features for %s API v%d.%d, %s, %s\n%s\n"),
         (pInParams->bUseHWLib) ? _T("hw") : _T("sw"), m_mfxVer.Major, m_mfxVer.Minor,
         CodecIdToStr(pInParams->CodecId).c_str(), EncmodeToStr(pInParams->nEncMode), MakeFeatureListStr(availableFeaures).c_str());
     if (!(availableFeaures & ENC_FEATURE_CURRENT_RC)) {
