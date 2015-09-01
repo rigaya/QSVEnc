f231782f708c602f046c13a0e863c6a6bcfe1142
rigaya <rigaya34589@live.jp>
2015-09-01 21:10:27 +0900

    mv-scalingが適切に設定されないのを修正。

--- QSVPipeline/src/pipeline_encode.cpp
+++ QSVPipeline/src/pipeline_encode.cpp
@@ -865,9 +865,9 @@ mfxStatus CEncodingPipeline::InitMfxEncParams(sInputParams *pInParams)
 
         //API v1.13の機能
         if (check_lib_version(m_mfxVer, MFX_LIB_VERSION_1_13)) {
-            m_CodingOption3.GlobalMotionBiasAdjustment = (mfxU16)((pInParams->bGlobalMotionAdjust) ? MFX_CODINGOPTION_ON : MFX_CODINGOPTION_OFF);
             m_CodingOption3.DirectBiasAdjustment       = (mfxU16)((pInParams->bDirectBiasAdjust)   ? MFX_CODINGOPTION_ON : MFX_CODINGOPTION_OFF);
-            if (pInParams->bDirectBiasAdjust)
+            m_CodingOption3.GlobalMotionBiasAdjustment = (mfxU16)((pInParams->bGlobalMotionAdjust) ? MFX_CODINGOPTION_ON : MFX_CODINGOPTION_OFF);
+            if (pInParams->bGlobalMotionAdjust)
                 m_CodingOption3.MVCostScalingFactor    = pInParams->nMVCostScaling;
         }
         m_EncExtParams.push_back((mfxExtBuffer *)&m_CodingOption3);
