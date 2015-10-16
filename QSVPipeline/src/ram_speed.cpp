982a70b2d4df90ffde02ddce0406c41644193793
rigaya <rigaya34589@live.jp>
2015-10-17 08:07:11 +0900

    ram_speedで<thread>を使用。

--- QSVPipeline/src/ram_speed.cpp
+++ QSVPipeline/src/ram_speed.cpp
@@ -11,12 +11,15 @@
 #define NOMINMAX
 #include <Windows.h>
 #include <stdio.h>
-#include <stdint.h>
+#include <cstdint>
 #include <vector>
 #include <numeric>
+#include <chrono>
+#include <climits>
 #include <algorithm>
-#include <process.h>
-#pragma comment(lib, "winmm.lib")
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 
 #include "cpu_info.h"
 #include "qsv_simd.h"
@@ -29,6 +32,13 @@ typedef struct {
     uint32_t physical_cores;
     double megabytes_per_sec;
 } RAM_SPEED_THREAD;
+
+
+typedef struct {
+    bool ready;
+    std::mutex mtx;
+    std::condition_variable cv;
+} RAM_SPEED_THREAD_WAKE;
 
 #ifdef __cplusplus
 extern "C" {
@@ -44,11 +54,8 @@ extern void __stdcall write_avx(uint8_t *dst, uint32_t size, uint32_t count_n);
 
 typedef void(__stdcall *func_ram_test)(uint8_t *dst, uint32_t size, uint32_t count_n);
 
-unsigned int __stdcall ram_speed_func(void *prm) {
+void ram_speed_func(RAM_SPEED_THREAD *thread_prm, RAM_SPEED_THREAD_WAKE *thread_wk) {
     const int TEST_COUNT = 4;
-
-    RAM_SPEED_THREAD *thread_prm = (RAM_SPEED_THREAD *)prm;
-
     uint32_t check_size_bytes = (thread_prm->check_size_bytes + 255) & ~255;
     const uint32_t test_kilo_bytes   = (uint32_t)(((thread_prm->mode == RAM_SPEED_MODE_READ) ? 1 : 0.5) * thread_prm->physical_cores * 1024 * 1024 / std::max(1.0, log2(check_size_bytes / 1024.0)) + 0.5);
     const uint32_t warmup_kilo_bytes = test_kilo_bytes * 2;
@@ -62,32 +69,33 @@ unsigned int __stdcall ram_speed_func(void *prm) {
     };
 
     const func_ram_test ram_test = RAM_TEST_LIST[avx][thread_prm->mode];
-    
-    int64_t start, fin, freq;
-    QueryPerformanceFrequency((LARGE_INTEGER *)&freq);
+
+    {
+        std::unique_lock<std::mutex> uniq_lk(thread_wk->mtx); // ここでロックされる
+        thread_wk->cv.wait(uniq_lk, [&thread_wk] { return thread_wk->ready; });
+    }
+
     ram_test(ptr, check_size_bytes, (int)(warmup_kilo_bytes * 1024.0 / check_size_bytes + 0.5));
     for (int i = 0; i < TEST_COUNT; i++) {
-        QueryPerformanceCounter((LARGE_INTEGER *)&start);
+        auto start = std::chrono::high_resolution_clock::now();
         ram_test(ptr, check_size_bytes, count_n);
-        QueryPerformanceCounter((LARGE_INTEGER *)&fin);
-        result[i] = fin - start;
+        auto fin = std::chrono::high_resolution_clock::now();
+        result[i] = std::chrono::duration_cast<std::chrono::microseconds>(fin - start).count();
     }
     ram_test(ptr, check_size_bytes, (int)(warmup_kilo_bytes * 1024.0 / check_size_bytes + 0.5));
     _aligned_free(ptr);
 
-    int64_t time_min = INT64_MAX;
+    int64_t time_min = LLONG_MAX;
     for (int i = 0; i < TEST_COUNT; i++)
         time_min = std::min(time_min, result[i]);
 
-    double time_ms = time_min * 1000.0 / freq;
-    thread_prm->megabytes_per_sec = (check_size_bytes * (double)count_n / (1024.0 * 1024.0)) / (time_ms * 0.001);
-    _endthreadex(0);
-    return 0;
+    thread_prm->megabytes_per_sec = (check_size_bytes * (double)count_n / (1024.0 * 1024.0)) / (time_min * 0.000001);
 }
 
 double ram_speed_mt(int check_size_kilobytes, int mode, int thread_n) {
-    std::vector<HANDLE> threads(thread_n, NULL);
+    std::vector<std::thread> threads(thread_n);
     std::vector<RAM_SPEED_THREAD> thread_prm(thread_n);
+    RAM_SPEED_THREAD_WAKE thread_wake;
     cpu_info_t cpu_info;
     get_cpu_info(&cpu_info);
     for (uint32_t i = 0; i < threads.size(); i++) {
@@ -95,22 +103,21 @@ double ram_speed_mt(int check_size_kilobytes, int mode, int thread_n) {
         thread_prm[i].mode = (mode == RAM_SPEED_MODE_RW) ? (i & 1) : mode;
         thread_prm[i].check_size_bytes = (check_size_kilobytes * 1024 / thread_n + 255) & ~255;
         thread_prm[i].thread_id = (i % cpu_info.physical_cores) * (cpu_info.logical_cores / cpu_info.physical_cores) + (int)(i / cpu_info.physical_cores);
-        threads[i] = (HANDLE)_beginthreadex(NULL, 0, ram_speed_func, &thread_prm[i], CREATE_SUSPENDED, NULL);
+        threads[i] = std::thread(ram_speed_func, &thread_prm[i], &thread_wake);
         //渡されたスレッドIDからスレッドAffinityを決定
         //特定のコアにスレッドを縛り付ける
-        SetThreadAffinityMask(threads[i], 1 << (int)thread_prm[i].thread_id);
+        SetThreadAffinityMask(threads[i].native_handle(), 1 << (int)thread_prm[i].thread_id);
         //高優先度で実行
-        SetThreadPriority(threads[i], THREAD_PRIORITY_HIGHEST);
+        SetThreadPriority(threads[i].native_handle(), THREAD_PRIORITY_HIGHEST);
     }
     
-    for (uint32_t i = 0; i < thread_prm.size(); i++) {
-        ResumeThread(threads[i]);
+    { //スレッドを起動
+        std::unique_lock<std::mutex> lock(thread_wake.mtx);
+        thread_wake.ready = true;
+        thread_wake.cv.notify_all();
     }
-    WaitForMultipleObjects((DWORD)threads.size(), &threads[0], TRUE, INFINITE);
-    for (auto th : threads) {
-        if (th) {
-            CloseHandle(th);
-        }
+    for (uint32_t i = 0; i < threads.size(); i++) {
+        threads[i].join();
     }
 
     double sum = 0.0;
