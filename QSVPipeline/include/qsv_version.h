c47dd5479c4f9c0f391a2fc1443453a912ffbfb4
rigaya <rigaya34589@live.jp>
2015-11-01 23:05:05 +0900

    QSVEnc.iniをutf-8にしてしまっていたのを修正。

--- QSVPipeline/include/qsv_version.h
+++ QSVPipeline/include/qsv_version.h
@@ -18,9 +18,9 @@
 
 #define MSDK_SAMPLE_VERSION MSDK_STRING(MFX_PRODUCT_VERSION)
 
-#define VER_FILEVERSION             0,2,19,0
-#define VER_STR_FILEVERSION          "2.19"
-#define VER_STR_FILEVERSION_TCHAR _T("2.19")
+#define VER_FILEVERSION             0,2,20,0
+#define VER_STR_FILEVERSION          "2.20"
+#define VER_STR_FILEVERSION_TCHAR _T("2.20")
 
 #ifdef _M_IX86
 #define BUILD_ARCH_STR _T("x86")
