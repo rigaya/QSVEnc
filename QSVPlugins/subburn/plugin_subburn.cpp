9907a488cf103614e0999b67d7077e33011defd6
rigaya <rigaya34589@live.jp>
2016-04-24 00:06:41 +0900

    字幕を読み込む際に、--vpp-sub-charsetが未指定の場合、日本語文字コードの自動判定を行うように。

--- QSVPlugins/subburn/plugin_subburn.cpp
+++ QSVPlugins/subburn/plugin_subburn.cpp
@@ -12,6 +12,8 @@
 #include <cstdio>
 #include <cstring>
 #include <map>
+#include <fstream>
+#include <iostream>
 #include "qsv_util.h"
 #include "qsv_simd.h"
 #include "qsv_osdep.h"
@@ -23,6 +25,141 @@
 #pragma comment(lib, "libass-5.lib")
 
 #pragma warning(disable : 4100)
+
+//日本語環境の一般的なコードページ一覧
+enum : uint32_t {
+    CODE_PAGE_SJIS        = 932, //Shift-JIS
+    CODE_PAGE_JIS         = 50220,
+    CODE_PAGE_EUC_JP      = 51932,
+    CODE_PAGE_UTF8        = 65001,
+    CODE_PAGE_UTF16_LE    = 1200, //WindowsのUnicode WCHAR のコードページ
+    CODE_PAGE_UTF16_BE    = 1201,
+    CODE_PAGE_US_ASCII    = 20127,
+    CODE_PAGE_WEST_EUROPE = 1252,  //厄介な西ヨーロッパ言語
+    CODE_PAGE_UNSET       = 0xffffffff,
+};
+
+//BOM文字リスト
+static const int MAX_UTF8_CHAR_LENGTH = 6;
+static const uint8_t UTF8_BOM[]     = { 0xEF, 0xBB, 0xBF };
+static const uint8_t UTF16_LE_BOM[] = { 0xFF, 0xFE };
+static const uint8_t UTF16_BE_BOM[] = { 0xFE, 0xFF };
+
+//ボム文字かどうか、コードページの判定
+static uint32_t check_bom(const void* chr) {
+    if (chr == nullptr) return CODE_PAGE_UNSET;
+    if (memcmp(chr, UTF16_LE_BOM, sizeof(UTF16_LE_BOM)) == 0) return CODE_PAGE_UTF16_LE;
+    if (memcmp(chr, UTF16_BE_BOM, sizeof(UTF16_BE_BOM)) == 0) return CODE_PAGE_UTF16_BE;
+    if (memcmp(chr, UTF8_BOM,     sizeof(UTF8_BOM))     == 0) return CODE_PAGE_UTF8;
+    return CODE_PAGE_UNSET;
+}
+
+static BOOL isJis(const void *str, uint32_t size_in_byte) {
+    static const uint8_t ESCAPE[][7] = {
+        //先頭に比較すべきバイト数
+        { 3, 0x1B, 0x28, 0x42, 0x00, 0x00, 0x00 },
+        { 3, 0x1B, 0x28, 0x4A, 0x00, 0x00, 0x00 },
+        { 3, 0x1B, 0x28, 0x49, 0x00, 0x00, 0x00 },
+        { 3, 0x1B, 0x24, 0x40, 0x00, 0x00, 0x00 },
+        { 3, 0x1B, 0x24, 0x42, 0x00, 0x00, 0x00 },
+        { 6, 0x1B, 0x26, 0x40, 0x1B, 0x24, 0x42 },
+        { 4, 0x1B, 0x24, 0x28, 0x44, 0x00, 0x00 },
+        { 0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 } //終了
+    };
+    const uint8_t * const str_fin = (const uint8_t *)str + size_in_byte;
+    for (const uint8_t *chr = (const uint8_t *)str; chr < str_fin; chr++) {
+        if (*chr > 0x7F)
+            return FALSE;
+        for (int i = 0; ESCAPE[i][0]; i++) {
+            if (str_fin - chr > ESCAPE[i][0] && 
+                memcmp(chr, &ESCAPE[i][1], ESCAPE[i][0]) == 0)
+                return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+static uint32_t isUTF16(const void *str, uint32_t size_in_byte) {
+    const uint8_t * const str_fin = (const uint8_t *)str + size_in_byte;
+    for (const uint8_t *chr = (const uint8_t *)str; chr < str_fin; chr++) {
+        if (chr[0] == 0x00 && str_fin - chr > 1 && chr[1] <= 0x7F)
+            return ((chr - (const uint8_t *)str) % 2 == 1) ? CODE_PAGE_UTF16_LE : CODE_PAGE_UTF16_BE;
+    }
+    return CODE_PAGE_UNSET;
+}
+
+static BOOL isASCII(const void *str, uint32_t size_in_byte) {
+    const uint8_t * const str_fin = (const uint8_t *)str + size_in_byte;
+    for (const uint8_t *chr = (const uint8_t *)str; chr < str_fin; chr++) {
+        if (*chr == 0x1B || *chr >= 0x80)
+            return FALSE;
+    }
+    return TRUE;
+}
+
+static uint32_t jpn_check(const void *str, uint32_t size_in_byte) {
+    int score_sjis = 0;
+    int score_euc = 0;
+    int score_utf8 = 0;
+    const uint8_t * const str_fin = (const uint8_t *)str + size_in_byte;
+    for (const uint8_t *chr = (const uint8_t *)str; chr < str_fin - 1; chr++) {
+        if ((0x81 <= chr[0] && chr[0] <= 0x9F) ||
+            (0xE0 <= chr[0] && chr[0] <= 0xFC) ||
+            (0x40 <= chr[1] && chr[1] <= 0x7E) ||
+            (0x80 <= chr[1] && chr[1] <= 0xFC)) {
+            score_sjis += 2; chr++;
+        }
+    }
+    for (const uint8_t *chr = (const uint8_t *)str; chr < str_fin - 1; chr++) {
+        if ((0xC0 <= chr[0] && chr[0] <= 0xDF) &&
+            (0x80 <= chr[1] && chr[1] <= 0xBF)) {
+            score_utf8 += 2; chr++;
+        } else if (
+            str_fin - chr > 2 &&
+            (0xE0 <= chr[0] && chr[0] <= 0xEF) &&
+            (0x80 <= chr[1] && chr[1] <= 0xBF) &&
+            (0x80 <= chr[2] && chr[2] <= 0xBF)) {
+            score_utf8 += 3; chr++;
+        }
+    }
+    for (const uint8_t *chr = (const uint8_t *)str; chr < str_fin - 1; chr++) {
+        if (((0xA1 <= chr[0] && chr[0] <= 0xFE) && (0xA1 <= chr[1] && chr[1] <= 0xFE)) ||
+            (chr[0] == 0x8E                     && (0xA1 <= chr[1] && chr[1] <= 0xDF))) {
+            score_euc += 2; chr++;
+        } else if (
+            str_fin - chr > 2 &&
+            chr[0] == 0x8F && 
+            (0xA1 <= chr[1] && chr[1] <= 0xFE) && 
+            (0xA1 <= chr[2] && chr[2] <= 0xFE)) {
+            score_euc += 3; chr += 2;
+        }
+    }
+    if (score_sjis > score_euc && score_sjis > score_utf8)
+        return CODE_PAGE_SJIS;
+    if (score_utf8 > score_euc && score_utf8 > score_sjis)
+        return CODE_PAGE_UTF8;
+    if (score_euc > score_sjis && score_euc > score_utf8)
+        return CODE_PAGE_EUC_JP;
+    return CODE_PAGE_UNSET;
+}
+
+static uint32_t get_code_page(const void *str, uint32_t size_in_byte) {
+    uint32_t ret = CODE_PAGE_UNSET;
+    if ((ret = check_bom(str)) != CODE_PAGE_UNSET)
+        return ret;
+
+    if (isJis(str, size_in_byte))
+        return CODE_PAGE_JIS;
+
+    if ((ret = isUTF16(str, size_in_byte)) != CODE_PAGE_UNSET)
+        return ret;
+
+    if (isASCII(str, size_in_byte))
+        return CODE_PAGE_US_ASCII;
+
+    return jpn_check(str, size_in_byte);
+}
+
 
 static bool check_libass_dll() {
 #if defined(_WIN32) || defined(_WIN64)
@@ -293,6 +430,33 @@ mfxStatus SubBurn::InitAvcodec(ProcessDataSubBurn *pProcData) {
         //ファイル読み込みの場合
         AddMessage(QSV_LOG_DEBUG, _T("trying to open subtitle file \"%s\""), pProcData->pFilePath);
 
+        if (pProcData->sCharEnc.length() == 0) {
+            FILE *fp = NULL;
+            if (_tfopen_s(&fp, pProcData->pFilePath, _T("rb")) || fp == NULL) {
+                AddMessage(QSV_LOG_ERROR, _T("error opening file: \"%s\"\n"), pProcData->pFilePath);
+                return MFX_ERR_NULL_PTR; // Couldn't open file
+            }
+
+            std::vector<char> buffer(256 * 1024, 0);
+            const auto readBytes = fread(buffer.data(), 1, sizeof(buffer[0]) * buffer.size(), fp);
+            fclose(fp);
+
+            const auto estCodePage = get_code_page(buffer.data(), (int)readBytes);
+            std::map<uint32_t, std::string> codePageMap = {
+                { CODE_PAGE_SJIS,     "CP932"       },
+                { CODE_PAGE_JIS,      "ISO-2022-JP" },
+                { CODE_PAGE_EUC_JP,   "EUC-JP"      },
+                { CODE_PAGE_UTF8,     "UTF-8"       },
+                { CODE_PAGE_UTF16_LE, "UTF-16LE"    },
+                { CODE_PAGE_UTF16_BE, "UTF-16BE"    },
+                { CODE_PAGE_US_ASCII, "ASCII"       },
+                { CODE_PAGE_UNSET,    ""            },
+            };
+            if (codePageMap.find(estCodePage) != codePageMap.end()) {
+                pProcData->sCharEnc = codePageMap[estCodePage];
+            }
+        }
+
         std::string filename_char;
         if (0 == tchar_to_string(pProcData->pFilePath, filename_char, CP_UTF8)) {
             AddMessage(QSV_LOG_ERROR, _T("failed to convert filename to utf-8 characters.\n"));
@@ -326,7 +490,6 @@ mfxStatus SubBurn::InitAvcodec(ProcessDataSubBurn *pProcData) {
         return MFX_ERR_UNSUPPORTED;
     }
     pProcData->nType = AV_CODEC_PROP_TEXT_SUB;
-    const AVCodecID codecId = AV_CODEC_ID_ASS;
     auto copy_subtitle_header = [](AVCodecContext *pDstCtx, const AVCodecContext *pSrcCtx) {
         if (pSrcCtx->subtitle_header_size) {
             pDstCtx->subtitle_header_size = pSrcCtx->subtitle_header_size;
@@ -360,9 +523,11 @@ mfxStatus SubBurn::InitAvcodec(ProcessDataSubBurn *pProcData) {
 
     int ret;
     AVDictionary *pCodecOpts = NULL;
-    if (0 > (ret = av_dict_set(&pCodecOpts, "sub_charenc", pProcData->sCharEnc.c_str(), 0))) {
-        AddMessage(QSV_LOG_ERROR, _T("failed to set \"sub_charenc\" option for subtitle decoder: %s\n"), qsv_av_err2str(ret).c_str());
-        return MFX_ERR_NULL_PTR;
+    if (pProcData->sCharEnc.length() > 0) {
+        if (0 > (ret = av_dict_set(&pCodecOpts, "sub_charenc", pProcData->sCharEnc.c_str(), 0))) {
+            AddMessage(QSV_LOG_ERROR, _T("failed to set \"sub_charenc\" option for subtitle decoder: %s\n"), qsv_av_err2str(ret).c_str());
+            return MFX_ERR_NULL_PTR;
+        }
     }
     if (0 > (ret = av_dict_set(&pCodecOpts, "sub_text_format", "ass", 0))) {
         AddMessage(QSV_LOG_ERROR, _T("failed to set \"sub_text_format\" option for subtitle decoder: %s\n"), qsv_av_err2str(ret).c_str());
@@ -378,6 +543,7 @@ mfxStatus SubBurn::InitAvcodec(ProcessDataSubBurn *pProcData) {
         pProcData->pOutCodecDecodeCtx->width, pProcData->pOutCodecDecodeCtx->height);
 #if 0
     //エンコーダを探す
+    const AVCodecID codecId = AV_CODEC_ID_ASS;
     if (NULL == (pProcData->pOutCodecEncode = avcodec_find_encoder(codecId))) {
         AddMessage(QSV_LOG_ERROR, errorMesForCodec(_T("failed to find encoder"), codecId));
         AddMessage(QSV_LOG_ERROR, _T("Please use --check-encoders to find available encoder.\n"));
