b23001948447683bde8496cd580e0b7d867a0380
rigaya <rigaya34589@live.jp>
2018-06-05 22:16:16 +0900

    浮動小数点がうまく読めないという指摘があったので、ロケールの指定をするようにした変更を元に戻す。

--- QSVEncC/QSVEncC.cpp
+++ QSVEncC/QSVEncC.cpp
@@ -1627,8 +1627,6 @@ int run(int argc, TCHAR *argv[]) {
 #if defined(_WIN32) || defined(_WIN64)
     if (check_locale_is_ja()) {
         _tsetlocale(LC_ALL, _T("Japanese"));
-    } else {
-        _tsetlocale(LC_ALL, _T(""));
     }
 #endif //#if defined(_WIN32) || defined(_WIN64)
     
