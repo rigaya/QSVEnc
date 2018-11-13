1d93d018cb36a024c6060fb4adb59ef9565e3fca
rigaya <rigaya34589@live.jp>
2018-11-13 21:03:51 +0900

    古いAvisynthを使うと正常に動作しなくなっていたのを修正。

--- QSVPipeline/rgy_input_avs.cpp
+++ QSVPipeline/rgy_input_avs.cpp
@@ -126,6 +126,7 @@ RGY_ERR RGYInputAvs::Init(const TCHAR *strFileName, VideoInfo *pInputInfo, const
         return RGY_ERR_INVALID_HANDLE;
     }
 
+    const auto interface_ver = (m_sAvisynth.f_is_420 && m_sAvisynth.f_is_422 && m_sAvisynth.f_is_444) ? AVISYNTH_INTERFACE_VERSION : 5;
     if (nullptr == (m_sAVSenv = m_sAvisynth.f_create_script_environment(AVISYNTH_INTERFACE_VERSION))) {
         AddMessage(RGY_LOG_ERROR, _T("failed to init avisynth enviroment.\n"));
         return RGY_ERR_INVALID_HANDLE;
@@ -270,7 +271,7 @@ RGY_ERR RGYInputAvs::LoadNextFrame(RGYFrame *pSurface) {
         dst_array, src_array,
         m_inputVideoInfo.srcWidth, m_sAvisynth.f_get_pitch_p(frame, AVS_PLANAR_Y), m_sAvisynth.f_get_pitch_p(frame, AVS_PLANAR_U),
         pSurface->pitch(), m_inputVideoInfo.srcHeight, m_inputVideoInfo.srcHeight, m_inputVideoInfo.crop.c);
-    
+
     m_sAvisynth.f_release_video_frame(frame);
 
     m_pEncSatusInfo->m_sData.frameIn++;
