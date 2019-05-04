2d527a1bc166ef4f680a23a3a19d458d76398cbd
rigaya <rigaya34589@live.jp>
2019-05-04 23:33:26 +0900

    msbuildでも並列ビルドできるように。

--- BuildParallel/buildRelease.bat
+++ BuildParallel/buildRelease.bat
@@ -1,3 +1,3 @@
 cd /d "%~dp0"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat" x64
-msbuild release.build.proj /m:3
+msbuild /m release.build.proj
