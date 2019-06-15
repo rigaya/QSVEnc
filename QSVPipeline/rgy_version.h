988f1795f74bcca0558b7bc3aba75835b6e8c008
rigaya <rigaya34589@live.jp>
2019-06-15 18:16:41 +0900

    ffmpegと関連dllを更新。(20180520→20190613)

--- QSVPipeline/rgy_version.h
+++ QSVPipeline/rgy_version.h
@@ -31,9 +31,9 @@
 
 #include "rgy_rev.h"
 
-#define VER_FILEVERSION             0,3,20,0
-#define VER_STR_FILEVERSION          "3.20"
-#define VER_STR_FILEVERSION_TCHAR _T("3.20")
+#define VER_FILEVERSION             0,3,21,0
+#define VER_STR_FILEVERSION          "3.21"
+#define VER_STR_FILEVERSION_TCHAR _T("3.21")
 
 #ifdef _M_IX86
 #define BUILD_ARCH_STR _T("x86")
