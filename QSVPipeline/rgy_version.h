51dd1bdcaa3c0c496227e34a9780877124042968
rigaya <rigaya34589@live.jp>
2019-08-16 19:11:53 +0900

    音声デコードで、ひとつの入力パケットから複数のフレームが得られる場合のtimestamp計算を修正。

--- QSVPipeline/rgy_version.h
+++ QSVPipeline/rgy_version.h
@@ -31,9 +31,9 @@
 
 #include "rgy_rev.h"
 
-#define VER_FILEVERSION             0,3,24,0
-#define VER_STR_FILEVERSION          "3.24"
-#define VER_STR_FILEVERSION_TCHAR _T("3.24")
+#define VER_FILEVERSION             0,3,25,0
+#define VER_STR_FILEVERSION          "3.25"
+#define VER_STR_FILEVERSION_TCHAR _T("3.25")
 
 #ifdef _M_IX86
 #define BUILD_ARCH_STR _T("x86")
