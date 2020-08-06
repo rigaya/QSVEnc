14f4b2d42aa004cc3bed624da77b4276f8f85a9a
rigaya <rigaya34589@live.jp>
2020-08-06 22:36:54 +0900

    使用するAvisynth.dllを指定するオプションを追加。( --avsdll )

--- QSVPipeline/rgy_version.h
+++ QSVPipeline/rgy_version.h
@@ -31,9 +31,9 @@
 
 #include "rgy_rev.h"
 
-#define VER_FILEVERSION             0,4,6,0
-#define VER_STR_FILEVERSION          "4.06"
-#define VER_STR_FILEVERSION_TCHAR _T("4.06")
+#define VER_FILEVERSION             0,4,7,0
+#define VER_STR_FILEVERSION          "4.07"
+#define VER_STR_FILEVERSION_TCHAR _T("4.07")
 
 #ifdef _M_IX86
 #define BUILD_ARCH_STR _T("x86")
