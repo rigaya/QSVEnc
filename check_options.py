e3c45b5603ec0f1223cca9d1cb93d6ea413ac4f6
rigaya <rigaya34589@live.jp>
2020-09-27 16:11:06 +0900

    check_option.pyのlinuxでの実行対象を変更。

--- check_options.py
+++ check_options.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
     md_files = []
     md_en_file = 'QSVEncC_Options.en.md'
     md_ja_file = 'QSVEncC_Options.ja.md'
-    exe_file = r'_build\x64\ReleaseStatic\QSVEncC64.exe' if os.name == 'nt' else './qsvencc64'
+    exe_file = r'_build\x64\ReleaseStatic\QSVEncC64.exe' if os.name == 'nt' else './qsvencc'
     
     iarg = 0
     while iarg < len(sys.argv):
