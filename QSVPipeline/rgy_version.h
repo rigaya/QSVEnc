028e35be71319fd02cf18d8cb18014a0552de9df
rigaya <rigaya34589@live.jp>
2021-05-03 13:53:04 +0900

    linuxでのマクロ変数が抜けていたのを修正。

--- QSVPipeline/rgy_version.h
+++ QSVPipeline/rgy_version.h
@@ -129,6 +129,7 @@ const char *get_encoder_version();
 #endif //#ifndef ENABLE_METRIC_FRAMEWORK
 
 #else //#if defined(WIN32) || defined(WIN64)
+#define D3D_SURFACES_SUPPORT 0
 #define MFX_D3D11_SUPPORT 0
 #define FOR_AUO 0
 #define ENABLE_METRIC_FRAMEWORK 0
