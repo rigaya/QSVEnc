972117a84080b86f61320b80cc393a55f1b73df8
rigaya <rigaya34589@live.jp>
2021-05-21 23:11:24 +0900

    arib_captionをtimed_id3に変換する機能を実装。 ( rigaya/NVEnc#335 )

--- QSVPipeline/rgy_version.h
+++ QSVPipeline/rgy_version.h
@@ -31,9 +31,9 @@
 
 #include "rgy_rev.h"
 
-#define VER_FILEVERSION             0,5,2,0
-#define VER_STR_FILEVERSION          "5.02"
-#define VER_STR_FILEVERSION_TCHAR _T("5.02")
+#define VER_FILEVERSION             0,5,3,1
+#define VER_STR_FILEVERSION          "5.03 beta1"
+#define VER_STR_FILEVERSION_TCHAR _T("5.03 beta1")
 
 #ifdef _M_IX86
 #define BUILD_ARCH_STR _T("x86")
