cdf853e5844371778cd41486b5c98575db188221
rigaya <rigaya34589@live.jp>
2021-07-27 22:34:43 +0900

    23d62c28 が不十分だったのを修正。( #59 )

--- QSVPipeline/rgy_filter_crop.cpp
+++ QSVPipeline/rgy_filter_crop.cpp
@@ -431,8 +431,8 @@ RGY_ERR RGYFilterCspCrop::convertCspFromYUV444(RGYFrameInfo *pOutputFrame, const
         RGYWorkSize local(32, 8);
         RGYWorkSize global(planeDstC.width >> 1, planeDstC.height);
         auto err = m_cropUV->kernel("kernel_crop_c_yuv444_nv12").config(queue, local, global, event).launch(
-            (cl_mem)planeDstC.ptr[0], planeDstC.pitch[0], planeDstC.width, planeDstC.height,
-            (cl_mem)planeSrcU.ptr[0], (cl_mem)planeSrcV.ptr[0], planeSrcU.pitch[0], planeSrcU.width >> 1, planeSrcU.height,
+            (cl_mem)planeDstC.ptr[0], planeDstC.pitch[0], planeDstC.width >> 1, planeDstC.height,
+            (cl_mem)planeSrcU.ptr[0], (cl_mem)planeSrcV.ptr[0], planeSrcU.pitch[0], planeSrcU.width, planeSrcU.height,
             pCropParam->crop.e.left, pCropParam->crop.e.up);
         if (err != RGY_ERR_NONE) {
             AddMessage(RGY_LOG_ERROR, _T("error at kernel_crop_c_yuv444_nv12 (convertCspFromYUV444(%s -> %s)): %s.\n"),
