ab6e1e1a6b349aff9b4fd6f92a5dde53c68212a6
rigaya <rigaya34589@live.jp>
2021-10-04 19:01:03 +0900

    端数の考慮を追加。

--- QSVPipeline/rgy_bitstream_avx2.cpp
+++ QSVPipeline/rgy_bitstream_avx2.cpp
@@ -45,15 +45,67 @@ static_assert(false, "do not forget to set /arch:AVX or /arch:AVX2 for this file
 #define CTZ64(x) __builtin_ctzll(x)
 #endif
 
+static inline __m256i _mm256_vsrli256_epi8(const __m256i& v, const int shift) {
+    static const uint8_t alignas(16) shufbtable[] = {
+        0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80,
+        0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80,
+        0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80,
+        0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80,
+        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+        0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+        0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80,
+        0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80,
+        0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80,
+        0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80,
+    };
+    __m256i mask = _mm256_broadcastsi128_si256(_mm_loadu_si128((const __m128i*)(shufbtable + shift + 32)));
+    __m256i a0 = _mm256_shuffle_epi8(v, mask);
+    __m256i a1 = _mm256_castsi128_si256(_mm_shuffle_epi8(_mm256_extracti128_si256(v, 1), _mm_loadu_si128((const __m128i*)(shufbtable + shift + 16))));
+    __m256i a2 = _mm256_permute2x128_si256(a1, a1, 0x80);
+    return _mm256_or_si256(a0, a2);
+}
+
+__m256i _mm256_loadu_si256_no_page_overread(const uint8_t *const data, const uint8_t *const data_fin) {
+    const size_t page_size = 4096;
+    const size_t load_size = 32; // 256bit
+    const auto size = data_fin - data;
+    size_t datapageaddress = ((size_t)data & (page_size - 1));
+    if (datapageaddress > (page_size - load_size) && (datapageaddress + size) <= page_size) { //ページ境界をまたぐ可能性があるか?
+        const auto loadaddress = (const uint8_t *const)((size_t)data & (~(load_size-1)));
+        const int shift = data - loadaddress; // ロードを引き戻す量
+        __m256i y0 = _mm256_loadu_si256((const __m256i*)loadaddress);
+        return _mm256_vsrli256_epi8(y0, shift);
+    } else {
+        return _mm256_loadu_si256((const __m256i*)data);
+    }
+}
+
 static inline int64_t memmem_avx2(const void *data_, const int64_t data_size, const void *target_, const int64_t target_size) {
     uint8_t *data = (uint8_t *)data_;
     const uint8_t *target = (const uint8_t *)target_;
     const __m256i target_first = _mm256_set1_epi8(target[0]);
     const __m256i target_last = _mm256_set1_epi8(target[target_size - 1]);
-
-    for (int64_t i = 0; i < data_size; i += 32) {
+    const int64_t fin = data_size - target_size + 1 - 32; // r1の32byteロードが安全に行える限界
+    //まずは単純なロードで行えるところまでループ
+    int64_t i = 0;
+    for (; i < fin; i += 32) {
         const __m256i r0 = _mm256_loadu_si256((const __m256i*)(data + i));
         const __m256i r1 = _mm256_loadu_si256((const __m256i*)(data + i + target_size - 1));
+        uint32_t mask = _mm256_movemask_epi8(_mm256_and_si256(_mm256_cmpeq_epi8(r0, target_first), _mm256_cmpeq_epi8(r1, target_last)));
+        while (mask != 0) {
+            const auto j = CTZ32(mask);
+            if (memcmp(data + i + j + 1, target + 1, target_size - 2) == 0) {
+                const auto ret = i + j;
+                return ret;
+            }
+            mask = CLEAR_LEFT_BIT(mask);
+        }
+    }
+    //確保されているメモリ領域のページ境界を考慮しながらロード
+    uint8_t *data_fin = data + data_size;
+    for (; i < data_size; i += 32) {
+        const __m256i r0 = _mm256_loadu_si256_no_page_overread(data + i, data_fin);
+        const __m256i r1 = _mm256_loadu_si256_no_page_overread(data + i + target_size - 1, data_fin);
         uint32_t mask = _mm256_movemask_epi8(_mm256_and_si256(_mm256_cmpeq_epi8(r0, target_first), _mm256_cmpeq_epi8(r1, target_last)));
         while (mask != 0) {
             const auto j = CTZ32(mask);
