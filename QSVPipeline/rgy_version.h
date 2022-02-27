fb71d4c0a8f76e81d48d84391098847f184c3725
rigaya <rigaya34589@live.jp>
2022-02-27 18:19:19 +0900

    appveyor.ymlを削除。お世話になりました。

--- QSVPipeline/rgy_version.h
+++ QSVPipeline/rgy_version.h
@@ -31,9 +31,9 @@
 
 #include "rgy_rev.h"
 
-#define VER_FILEVERSION             0,6,8,3
-#define VER_STR_FILEVERSION          "6.08v3"
-#define VER_STR_FILEVERSION_TCHAR _T("6.08v3")
+#define VER_FILEVERSION             0,6,9,0
+#define VER_STR_FILEVERSION          "6.09"
+#define VER_STR_FILEVERSION_TCHAR _T("6.09")
 
 #ifdef _M_IX86
 #define BUILD_ARCH_STR _T("x86")
