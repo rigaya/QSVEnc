8041fb7e2271cdac7efe94e68fc43ec3aadd1c38
rigaya <rigaya34589@live.jp>
2022-04-07 22:05:26 +0900

    ビルド用のバッチファイルもVS2022に移行。

--- BuildParallel/buildAll.bat
+++ BuildParallel/buildAll.bat
@@ -1,3 +1,3 @@
 cd /d "%~dp0"
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat" x64
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" x64
 msbuild build.all.proj /m
