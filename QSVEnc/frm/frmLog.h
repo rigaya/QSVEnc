ad51d1a84f8ff011834921901b31336aa1003bac
rigaya <rigaya34589@live.jp>
2022-05-19 13:51:27 +0900

    ScrollToCaret()を使用しないように。
    頻繁に呼び出すと、最終的にSystem.AccessViolationExceptionが出ることがあるという報告合があるため。

--- QSVEnc/frm/frmLog.h
+++ QSVEnc/frm/frmLog.h
@@ -664,9 +664,9 @@ private: System::Windows::Forms::ToolStripMenuItem^  ToolStripMenuItemEncPause;
                     richTextLog->SelectionLength = richTextLog->Text->Length;
                     richTextLog->SelectionColor = log_color_text[log_type_index];
                     richTextLog->AppendText(L"auo [" + log_type[log_type_index] + L"]: " + str + L"\n");
-                    richTextLog->SelectionStart = richTextLog->Text->Length;
+                    //richTextLog->SelectionStart = richTextLog->Text->Length;
                     if (!wine_compatible_mode) {
-                        richTextLog->ScrollToCaret();
+                        //richTextLog->ScrollToCaret(); // 頻繁に呼び出すと、最終的にSystem.AccessViolationExceptionが出ることがあるらしい
                     }
                     richTextLog->ResumeLayout();
                 }
@@ -689,9 +689,9 @@ private: System::Windows::Forms::ToolStripMenuItem^  ToolStripMenuItemEncPause;
                     richTextLog->SelectionLength = richTextLog->Text->Length;
                     richTextLog->SelectionColor = log_color_text[log_type_index];
                     richTextLog->AppendText(str + L"\n");
-                    richTextLog->SelectionStart = richTextLog->Text->Length;
+                    //richTextLog->SelectionStart = richTextLog->Text->Length;
                     if (!wine_compatible_mode) {
-                        richTextLog->ScrollToCaret();
+                        //richTextLog->ScrollToCaret(); // 頻繁に呼び出すと、最終的にSystem.AccessViolationExceptionが出ることがあるらしい
                     }
                     richTextLog->ResumeLayout();
                 }
@@ -699,11 +699,19 @@ private: System::Windows::Forms::ToolStripMenuItem^  ToolStripMenuItemEncPause;
         }
     public:
         System::Void FlushAudioLogCache() {
+            richTextLog->SuspendLayout();
             for (int i = 0; i < AudioParallelCache.Count; i++) {
-                (AudioParallelCache[i].type)
-                    ? WriteLogLine(AudioParallelCache[i].str, AudioParallelCache[i].log_type_index)
-                    : WriteLogAuoLine(AudioParallelCache[i].str, AudioParallelCache[i].log_type_index);
+                const int log_type_index = clamp(AudioParallelCache[i].log_type_index, LOG_INFO, LOG_ERROR);
+                richTextLog->SelectionStart = richTextLog->Text->Length;
+                richTextLog->SelectionLength = richTextLog->Text->Length;
+                richTextLog->SelectionColor = log_color_text[log_type_index];
+                if (AudioParallelCache[i].type) {
+                    richTextLog->AppendText(AudioParallelCache[i].str + L"\n");
+                } else {
+                    richTextLog->AppendText(L"auo [" + log_type[log_type_index] + L"]: " + AudioParallelCache[i].str + L"\n");
+                }
             }
+            richTextLog->ResumeLayout();
             AudioParallelCache.Clear();
         }
     private:
