46957a87773ad7617237f365c6bde41b5dcb850e
rigaya <rigaya34589@live.jp>
2022-07-03 17:57:18 +0900

    AV1 hw デコードがQSVEnc 6.10からエラー終了する場合があったのを回避。

--- QSVPipeline/rgy_version.h
+++ QSVPipeline/rgy_version.h
@@ -31,9 +31,9 @@
 
 #include "rgy_rev.h"
 
-#define VER_FILEVERSION             0,7,5,0
-#define VER_STR_FILEVERSION          "7.05"
-#define VER_STR_FILEVERSION_TCHAR _T("7.05")
+#define VER_FILEVERSION             0,7,6,0
+#define VER_STR_FILEVERSION          "7.06"
+#define VER_STR_FILEVERSION_TCHAR _T("7.06")
 
 #ifdef _M_IX86
 #define BUILD_ARCH_STR _T("x86")
