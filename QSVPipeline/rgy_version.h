a07af1e2a30b072e38b36754e3a96900ba9b5027
rigaya <rigaya34589@live.jp>
2023-01-22 15:34:47 +0900

    AV1のmaster-displayの取り扱いを改善。

--- QSVPipeline/rgy_version.h
+++ QSVPipeline/rgy_version.h
@@ -31,9 +31,9 @@
 
 #include "rgy_rev.h"
 
-#define VER_FILEVERSION             0,7,26,0
-#define VER_STR_FILEVERSION          "7.26"
-#define VER_STR_FILEVERSION_TCHAR _T("7.26")
+#define VER_FILEVERSION             0,7,27,0
+#define VER_STR_FILEVERSION          "7.27"
+#define VER_STR_FILEVERSION_TCHAR _T("7.27")
 
 #ifdef _M_IX86
 #define BUILD_ARCH_STR _T("x86")
