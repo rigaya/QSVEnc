2368475948a916bcad9bee1b6b18b0e880dd43ad
rigaya <rigaya34589@live.jp>
2023-06-19 18:53:52 +0900

    faw処理で異常終了する場合があったのを修正。

--- QSVPipeline/rgy_version.h
+++ QSVPipeline/rgy_version.h
@@ -31,9 +31,9 @@
 
 #include "rgy_rev.h"
 
-#define VER_FILEVERSION             0,7,44,0
-#define VER_STR_FILEVERSION          "7.44"
-#define VER_STR_FILEVERSION_TCHAR _T("7.44")
+#define VER_FILEVERSION             0,7,45,0
+#define VER_STR_FILEVERSION          "7.45"
+#define VER_STR_FILEVERSION_TCHAR _T("7.45")
 
 #ifdef _M_IX86
 #define BUILD_ARCH_STR _T("x86")
